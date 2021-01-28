6bb1ac994c9ade96060dd4432799236f5a87b9d6
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-01-28 22:09:00 +0700

    Add solution for problem `B - Nezzar and Binary String` Link: https://codeforces.com/contest/1477/problem/B Tags: data structures, greedy, *1900 Ref: https://codeforces.com/contest/1477/submission/105706190

--- codeforces/1477/B.cpp
+++ codeforces/1477/B.cpp
@@ -37,7 +37,7 @@ using namespace __gnu_pbds;
 //min/max redefines, so i dont have to resolve annoying compile errors.
 #define min(a,b) (((a)<(b))?(a):(b))
 #define max(a,b) (((a)>(b))?(a):(b))
- 
+
 //Constants.
 #define MOD (ll(998244353))
 #define MAX 300001
