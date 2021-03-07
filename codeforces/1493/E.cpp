8975ffd9c667e2528f3227145772ea13347ddc7e
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-03-07 11:34:00 +0700

    Add solution for problem `E - Enormous XOR` Link: https://codeforces.com/contest/1493/problem/E Tags: bitmasks, constructive algorithms, greedy, math, strings, two pointers, *2600 Ref: https://codeforces.com/contest/1493/submission/109304924

--- codeforces/1493/E.cpp
+++ codeforces/1493/E.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
-using namespace std;string a,b;int n,m,i,j,k;char x='0',y='1';int main(){cin>>n>>a>>b;if(a[0]-b[0]){for(auto g:a)cout<<1;return 0;}if(b[n-1]==y){cout<<b;return 0;}for(j=0;j<2;j++){if(a==b)break;for(i=n-1;i>=0;i--)if(b[i]==x)b[i]=y;else{b[i]=x;break;}}if(j==2)j++;b="0"+b;for (j=j;j;j--){for(i=n;i>=0;i--)if(b[i]==y)b[i]=x;else{b[i]=y;break;}}if(b[0]==x)b.erase(b.begin());cout<<b;}
+using namespace std;
+string a,b;
+int n,m,i,j,k,t,t1;
+int main()
+{
+    cin>>n;
+    cin>>a>>b;
+    if (a[0]-b[0]) {for (auto g : a) cout<<1; return 0;}
+    if (b[n-1]=='1') {cout<<b; return 0;}
+    for (j=0;j<2;j++)
+    {
+        if (a==b) break;
+        for (i=n-1;i>=0;i--) if (b[i]=='0') b[i]='1'; else{b[i]='0'; break;}
+    }
+    if (j==2) j++;
+    b="0"+b;
+    for (j=j;j;j--)
+    {
+        for (i=n;i>=0;i--) if (b[i]=='1') b[i]='0'; else{b[i]='1'; break;}
+    }
+    if (b[0]=='0') b.erase(b.begin());
+    cout<<b;
+}
