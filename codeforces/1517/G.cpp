95b2cc51f374688067678baf5fecff04e022c5e8
NhatMinh0208 <minhkhicon2468@gmail.com>
2021-04-27 23:09:00 +0700

    Add solution for problem `G - Starry Night Camping` Link: https://codeforces.com/contest/1517/problem/G Tags: constructive algorithms, flows, graphs, *3300 Ref: https://codeforces.com/contest/1517/submission/114381275

---
+++ codeforces/1517/G.cpp
@@ -0,0 +1,317 @@
+/*
+	Normie's Template v2.2
+	Changes:
+	Added modulo binpow and inverse.
+*/
+ 
+// Standard library in one include.
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// ordered_set library.
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set(el) tree<el,null_type,less<el>,rb_tree_tag,tree_order_statistics_node_update>
+ 
+// AtCoder library. (Comment out these two lines if you're not submitting in AtCoder.) (Or if you want to use it in other judges, run expander.py first.)
+
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <queue>
+#include <vector>
+
+
+#include <vector>
+
+namespace atcoder {
+
+namespace internal {
+
+template <class T> struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
+    }
+    void pop() { pos++; }
+};
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class Cap> struct mf_graph {
+  public:
+    mf_graph() : _n(0) {}
+    explicit mf_graph(int n) : _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        int from_id = int(g[from].size());
+        int to_id = int(g[to].size());
+        if (from == to) to_id++;
+        g[from].push_back(_edge{to, to_id, cap});
+        g[to].push_back(_edge{from, from_id, 0});
+        return m;
+    }
+
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        std::vector<int> level(_n), iter(_n);
+        internal::simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) return res;
+            }
+            level[v] = _n;
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit - flow);
+            if (!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        internal::simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
+};
+
+}  // namespace atcoder
+
+using namespace atcoder;
+ 
+//Pragmas (Comment out these three lines if you're submitting in szkopul.)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast,unroll-loops,tree-vectorize")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+ 
+//File I/O.
+#define FILE_IN "cseq.inp"
+#define FILE_OUT "cseq.out"
+#define ofile freopen(FILE_IN,"r",stdin);freopen(FILE_OUT,"w",stdout)
+ 
+//Fast I/O.
+#define fio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define nfio cin.tie(0);cout.tie(0)
+#define endl "\n"
+ 
+//Order checking.
+#define ord(a,b,c) ((a>=b)and(b>=c))
+ 
+//min/max redefines, so i dont have to resolve annoying compile errors.
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+
+// Fast min/max assigns to use with AVX.
+// Requires g++ 9.2.0.
+template<typename T>
+__attribute__((always_inline)) void chkmin(T& a, const T& b) {
+    a=(a<b)?a:b;
+}
+
+template<typename T>
+__attribute__((always_inline)) void chkmax(T& a, const T& b) {
+    a=(a>b)?a:b;
+}
+ 
+//Constants.
+#define MOD (ll(998244353))
+#define MAX 300001
+#define mag 320
+const long double PI=3.14159265358979;
+ 
+//Pairs and 3-pairs.
+#define p1 first
+#define p2 second.first
+#define p3 second.second
+#define fi first
+#define se second
+#define pii(element_type) pair<element_type,element_type>
+#define piii(element_type) pair<element_type,pii(element_type)>
+ 
+//Quick power of 2.
+#define pow2(x) (ll(1)<<x)
+ 
+//Short for-loops.
+#define ff(i,__,___) for(int i=__;i<=___;i++)
+#define rr(i,__,___) for(int i=__;i>=___;i--)
+ 
+//Typedefs.
+#define bi BigInt
+typedef long long ll;
+typedef long double ld;
+typedef short sh;
+
+// Binpow and stuff
+ll BOW(ll a, ll x, ll p)
+{
+	if (!x) return 1;
+	ll res=BOW(a,x/2,p);
+	res*=res;
+	res%=p;
+	if (x%2) res*=a;
+	return res%p;
+}
+ll INV(ll a, ll p)
+{
+	return BOW(a,p-2,p);
+}
+//---------END-------//
+vector<ll> vec;
+ll n,m,i,j,k,t,t1,u,v,a,b;
+ll x[1001],y[1001];
+int main()
+{
+	fio;
+    cin>>n;
+    mf_graph<ll> gr(2*n+2);
+    for (i=0;i<n;i++)
+    {
+        cin>>x[i]>>y[i]>>u;
+        v+=u;
+        gr.add_edge(i*2,i*2+1,u);
+    }
+    for (i=0;i<n;i++)
+    {
+        if ((x[i]%2)and(y[i]%2))
+        {
+            gr.add_edge(2*n,i*2,1e12+1);
+            for (j=0;j<n;j++)
+            {
+                if ((abs(x[j]-x[i])==1)and(y[j]==y[i])) 
+                gr.add_edge(i*2+1,j*2,1e12+1);
+            }
+        }
+        if ((x[i]%2==0)and(y[i]%2))
+        {
+            for (j=0;j<n;j++)
+            {
+                if ((abs(y[j]-y[i])==1)and(x[i]==x[j]))
+                gr.add_edge(i*2+1,j*2,1e12+1);
+            }
+        }
+        if ((x[i]%2==0)and(y[i]%2==0))
+        {
+            for (j=0;j<n;j++)
+            {
+                if ((abs(x[j]-x[i])==1)and(y[j]==y[i])) 
+                gr.add_edge(i*2+1,j*2,1e12+1);
+            }
+        }
+        if ((x[i]%2)and(y[i]%2==0))
+        {
+            gr.add_edge(i*2+1,2*n+1,1e12+1);
+        }
+    }
+    cout<<v-gr.flow(2*n,2*n+1);
+}
